e0a3dda75095a07098bbda0feaebf5c2428f4e98
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-17 04:31:40 -0600

    cleanup

--- entrypoint-xmrig.sh
+++ entrypoint-xmrig.sh
@@ -6,23 +6,5 @@ set -euxo pipefail
 exec 0<&-          # close stdin
 exec 2>&1          # redirect stderr to stdout
 renice -n -20 "$$" || : # max prio
-/usr/local/bin/xmrig             \
--c "/conf.d/$1.json"
-#-u "$1"                          \
-#-p docker                        \
-#-a cryptonightv7                 \
-#-o gulf.moneroocean.stream:10032 \
-#--keepalive                      \
-#--rig-id=docker                  \
-#--donate-level=0                 \
-#--cpu-priority 5                 \
-#--cpu-no-yield                   \
-#--nicehash                       \
-#--randomx-init=`nproc`           \
-#--randomx-mode=fast              \
-#--randomx-1gb-pages              \
-#--cuda                           \
-#--cuda-loader /usr/local/lib/libxmrig-cuda.so
-##--pause-on-battery
-##-c "/conf.d/$1.json"
+/usr/local/bin/xmrig -c "/conf.d/$1.json"
 
