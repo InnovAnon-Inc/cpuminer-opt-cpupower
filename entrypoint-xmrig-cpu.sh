00a091ee22e11607a8c485b25d65f440d6b00ab3
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-17 03:50:42 -0600

    backup

--- entrypoint-xmrig-cpu.sh
+++ entrypoint-xmrig-cpu.sh
@@ -7,15 +7,19 @@ exec 0<&-          # close stdin
 exec 2>&1          # redirect stderr to stdout
 renice -n -20 "$$" || : # max prio
 /usr/local/bin/xmrig             \
--u "$1"                          \
--p docker                        \
--a cryptonightv7                 \
--o gulf.moneroocean.stream:10032 \
---keepalive                      \
---donate-level=0                 \
---cpu-priority 5                 \
---cpu-no-yield                   \
---nicehash
-#--pause-on-battery
-#-c "/conf.d/$1.json"
+-c "/conf.d/$1.json"
+#-u "$1"                          \
+#-p docker                        \
+#-a cryptonightv7                 \
+#-o gulf.moneroocean.stream:10032 \
+#--keepalive                      \
+#--rig-id=docker                  \
+#--donate-level=0                 \
+#--cpu-priority 5                 \
+#--cpu-no-yield                   \
+#--nicehash                       \
+#--randomx-init=`nproc`           \
+#--randomx-mode=fast              \
+#--randomx-1gb-pages
+##--pause-on-battery
 
