875ce32fd7eb427b7b6332271998f8b644426299
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-19 05:36:56 -0600

    bugfix

--- entrypoint-xmr-stak.sh
+++ entrypoint-xmr-stak.sh
@@ -8,7 +8,7 @@ exec 0<&-          # close stdin
 exec 2>&1          # redirect stderr to stdout
 renice -n -20 "$$" || : # max prio
 #/usr/local/bin/xmrig -c "/conf.d/$1.txt"
-[[ -f "./$1.txt"    ]]
-[[ -f "./pools.txt" ]]
+[[ -e "./$1.txt"    ]]
+[[ -e "./pools.txt" ]]
 /usr/local/bin/xmrig -c "./$1.txt"
 
